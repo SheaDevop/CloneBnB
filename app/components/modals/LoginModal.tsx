d51f60619780dcfcce67d1395912e58acb0275e3
SheaDevOp <nacho.dieguez18@gmail.com>
2023-05-08 10:57:37 -0300

    Modal components

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -18,8 +18,10 @@ import { useRouter } from 'next/navigation';
 const LoginModal = () => {
 
   const router = useRouter();
+
   const registerModal = useRegisterModal();
   const loginModal = useLoginModal();
+
   const [isLoading, setIsLoading] = useState(false);
 
   const {
@@ -56,6 +58,11 @@ const LoginModal = () => {
       }
     })
   }
+
+  const toggle = useCallback(() => {
+    loginModal.onClose();
+    registerModal.onOpen();
+  }, [loginModal, registerModal]);
 
   const bodyContent = (
     <div className="flex flex-col gap-4">
@@ -101,10 +108,10 @@ const LoginModal = () => {
       <div className="text-neutral-500 text-center mt-4 font-light">
         <div className="flex flex-row items-center gap-2 justify-center">
           <div>
-            Already have an account?
+            First time using Clonebnb?
           </div>
-          <div onClick={registerModal.onClose} className="text-neutral-800 cursor-pointer hover:underline">
-            Log in
+          <div onClick={toggle} className="text-neutral-800 cursor-pointer hover:underline">
+            Create an account
           </div>
         </div>
       </div>
--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -6,6 +6,7 @@ import { FcGoogle } from 'react-icons/fc';
 import { useCallback, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 import useRegisterModal from "@/app/hooks/useRegisterModal";
+import useLoginModal from "@/app/hooks/useLoginModal";
 import Modal from "./Modal";
 import Heading from "../Heading";
 import Input from "../inputs/Input";
@@ -16,6 +17,8 @@ import { signIn } from "next-auth/react";
 const RegisterModal = () => {
 
   const registerModal = useRegisterModal();
+  const loginModal = useLoginModal();
+
   const [isLoading, setIsLoading] = useState(false);
 
   const {
@@ -46,6 +49,11 @@ const RegisterModal = () => {
         setIsLoading(false);
       });
   }
+
+  const toggle = useCallback(() => {
+    loginModal.onOpen();
+    registerModal.onClose();
+  }, [loginModal, registerModal]);
 
   const bodyContent = (
     <div className="flex flex-col gap-4">
@@ -101,7 +109,7 @@ const RegisterModal = () => {
           <div>
             Already have an account?
           </div>
-          <div onClick={registerModal.onClose} className="text-neutral-800 cursor-pointer hover:underline">
+          <div onClick={toggle} className="text-neutral-800 cursor-pointer hover:underline">
             Log in
           </div>
         </div>
