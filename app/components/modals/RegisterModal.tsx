bbfce50800816480e55821cb98624c3302ef4ef0
SheaDevOp <nacho.dieguez18@gmail.com>
2023-05-09 18:00:54 -0300

    RegisterModal

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -41,6 +41,7 @@ const RegisterModal = () => {
     axios.post('/api/register', data)
       .then(() => {
         registerModal.onClose();
+        loginModal.onOpen();
       })
       .catch((error) => {
         toast.error(error.message)
